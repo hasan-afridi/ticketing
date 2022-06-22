b4e94654bdf7e6aafd85e2de4b0102cdd573d2e2
hasan-afridi <hasan.afridi@truepill.com>
2022-06-22 17:07:02 +0100

    signup page send request to backend route

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,12 +1,22 @@
 import { useState } from "react";
+import axios from "axios";
 
 const signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    try {
+      const res = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+      console.log(res.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -29,6 +39,18 @@ const signup = () => {
           className="form-control"
         ></input>
       </div>
+
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Oops..</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
       <button className="btn btn-primary">Sign up</button>
     </form>
   );
