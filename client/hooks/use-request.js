24c3fdf6a898b171cc01753e3054f1b9b2200cb9
hasan-afridi <hasan.afridi@truepill.com>
2022-06-24 16:24:37 +0100

    add axios requests to landingpage

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 import { useState } from "react";
 
-const useRequest = ({ url, method, body }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async () => {
     try {
       setErrors(null);
       const res = await axios[method](url, body);
+      if (onSuccess) {
+        onSuccess(res.data);
+      }
       return res.data;
     } catch (err) {
       setErrors(
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,33 @@
-const landing = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  // axios.get("/api/users/currentuser").catch((err) => {
+  //   console.log(err.message);
+  // });
   return <h1>Landing Page</h1>;
 };
 
-export default landing;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    //we are on the server
+    const { data } = await axios
+      .get(
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+        {
+          headers: req.headers,
+        }
+      )
+      .catch((err) => {
+        console.log(err);
+      });
+    return data;
+  } else {
+    //we are on the browser
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
