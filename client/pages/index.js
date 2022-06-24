60b700e590f2adc0a3793b5b1a793a2d8dfad73c
hasan-afridi <hasan.afridi@truepill.com>
2022-06-24 16:53:24 +0100

    extract client selection logic into helper function

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,33 +1,14 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-  // axios.get("/api/users/currentuser").catch((err) => {
-  //   console.log(err.message);
-  // });
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    //we are on the server
-    const { data } = await axios
-      .get(
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-        {
-          headers: req.headers,
-        }
-      )
-      .catch((err) => {
-        console.log(err);
-      });
-    return data;
-  } else {
-    //we are on the browser
-    const { data } = await axios.get("/api/users/currentuser");
-    return data;
-  }
-  return {};
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
